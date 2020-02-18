a1451191c799735533e11a33a499cadf3a179a2f
kretawiweka <ekanuraga@gmail.com>
2020-02-18 16:59:54 +0700

    move file structure

---
+++ craft-example/src/containers/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Title from './style';
+
+const Home = () => <Title>Home</Title>;
+
+export default Home;
---
+++ craft-example/src/containers/Home/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 14px;
+`;
+
+export default Title;
